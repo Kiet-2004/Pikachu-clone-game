7becd37b4ef854bbae63658448bc5481cb8f5e35
Nguyễn Anh Kiệt <61195268+Kiet-2004@users.noreply.github.com>
2023-03-22 19:30:17 +0700

    Delete nightmare.h

--- nightmare.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void deleteNightmare(int m, int n, bool **&nightmare);
-void generateNightmare(int m, int n, bool **&nightmare);
-void resetNightmare(int m, int n, bool **nightmare);
